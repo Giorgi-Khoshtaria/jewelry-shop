94bc857b9e27a50dfc4ac5bf6ba99ec6019e750c
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2025-03-18 13:06:20 +0400

    feat: add max with adn second font

--- jewelery-shop-front/src/layout/Layout.tsx
+++ jewelery-shop-front/src/layout/Layout.tsx
@@ -5,7 +5,7 @@ import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-    <div className=" flex flex-col min-h-screen bg-tahiti">
+    <div className=" flex flex-col  bg-tahiti">
       <Header />
       <Outlet />
       <Footer />
