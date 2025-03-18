d29a10e2b0fa7a237af55af8a30b0806130ff607
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2025-03-18 14:42:51 +0400

    feat:add header

--- jewelery-shop-front/tailwind.config.js
+++ jewelery-shop-front/tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Make sure all your files are included
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: "#1E40AF", // Custom primary color
-        secondary: "#9333EA", // Custom secondary color
-        danger: "#DC2626", // Custom danger color
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
