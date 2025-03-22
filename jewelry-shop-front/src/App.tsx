17723f40cca4d20461ea3032d2d657bab233023d
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2025-03-22 23:36:45 +0400

    feat: add login, registration , forgot password files adn create path

--- jewelry-shop-front/src/App.tsx
+++ jewelry-shop-front/src/App.tsx
@@ -3,6 +3,9 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./Pages/Home/Home";
+import Login from "./Components/Auth/Login";
+import Registration from "./Components/Auth/Registration";
+import ForgotPassword from "./Components/Auth/ForgotPassword";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,18 @@ const router = createBrowserRouter([
         element: <Home />,
       },
     ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/registration",
+    element: <Registration />,
+  },
+  {
+    path: "/forgot-password",
+    element: <ForgotPassword />,
   },
 ]);
 function App() {
