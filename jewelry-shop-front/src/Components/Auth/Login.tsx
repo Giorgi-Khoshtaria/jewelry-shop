6bc91001c73a874cd8422694985878ba2cd8af41
giorgi khoshtaria <99177635+g1org11@users.noreply.github.com>
2025-03-23 22:29:00 +0400

    feat: add responsive on login page

--- jewelry-shop-front/src/Components/Auth/Login.tsx
+++ jewelry-shop-front/src/Components/Auth/Login.tsx
@@ -2,23 +2,23 @@ import React from "react";
 
 function Login() {
   return (
-    <div className="w-full  flex justify-center items-center min-h-screen">
-      <div className=" bg-Gray shadow p-12 rounded-2xl flex flex-col items-center">
+    <div className="w-full p-6 flex justify-center items-center min-h-screen">
+      <div className=" bg-Gray shadow p-12 rounded-2xl flex flex-col items-center max-md:w-full">
         <h1 className="text-3xl font-medium leading-11 text-black">Login</h1>
-        <form action="" className="flex flex-col items-start">
+        <form action="" className="flex flex-col items-start max-md:w-full">
           <input
             type="email"
             id="email"
             required
             placeholder="Email"
-            className="mt-10 py-3 w-[500px] border-b border-gray-400 focus:outline-none placeholder:text-base placeholder:font-normal placeholder:leading-[27px]"
+            className="mt-10 py-3 w-[500px] max-md:w-full border-b border-gray-400 focus:outline-none placeholder:text-base placeholder:font-normal placeholder:leading-[27px]"
           />
           <input
             type="password"
             id="password"
             required
             placeholder="Password"
-            className="mt-6 py-3 w-[500px] border-b border-gray-400 focus:outline-none placeholder:text-base placeholder:font-normal placeholder:leading-[27px]"
+            className="mt-6 py-3 w-[500px] max-md:w-full border-b border-gray-400 focus:outline-none placeholder:text-base placeholder:font-normal placeholder:leading-[27px]"
           />
           <div className="flex items-center mt-4">
             <input type="checkbox" id="rememberMe" className="mr-2" />
@@ -28,7 +28,7 @@ function Login() {
           </div>
           <button
             type="submit"
-            className="mt-6 py-3 w-[500px] bg-black text-white rounded-md"
+            className="mt-6 py-3 w-[500px] max-md:w-full bg-black text-white rounded-md"
           >
             SIGN IN
           </button>
